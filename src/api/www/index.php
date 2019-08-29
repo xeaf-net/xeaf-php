7a4f233bd71f5e85ba7fea7bd278969ae9ac7dae
Nick V. Anokhin <n.anokhin@xeaf.net>
2019-08-29 11:18:17 +0300

    Создан и настроен прототип проекта

---
+++ src/api/www/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * index.php
+ *
+ * Файл является неотъемлемой частью проекта XEAF-PHP
+ *
+ * @author    Николай В. Анохин <n.anokhin@xeaf.net>
+ * @copyright 2019 XEAF.NET Group
+ *
+ * @license   Apache 2.0
+ */
+namespace XEAF\API\WWW;
+
+print 'XEAF Version: ' . __XEAF_VERSION__;
---
+++ src/autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * autoload.php
+ *
+ * Файл является неотъемлемой частью проекта XEAF-PHP
+ *
+ * @author    Николай В. Анохин <n.anokhin@xeaf.net>
+ * @copyright 2019 XEAF.NET Group
+ *
+ * @license   Apache 2.0
+ */
+namespace XEAF;
+
+/**
+ * Номер версии библиотеки
+ */
+define('__XEAF_VERSION__', '7.1.0-dev');
