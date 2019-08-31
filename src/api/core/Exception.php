333c02874b3159d098c16f27c1e785c60e845c7c
n.anokhin <n.anokhin@xeaf.net>
2019-08-31 19:56:29 +0300

    Настроено тестирование

---
+++ src/api/core/Exception.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Exception.php
+ *
+ * Файл является неотъемлемой частью проекта XEAF-PHP
+ *
+ * @author    Николай В. Анохин <n.anokhin@xeaf.net>
+ * @copyright 2019 XEAF.NET Group
+ *
+ * @license   Apache 2.0
+ */
+namespace XEAF\API\Core;
+
+/**
+ * Реализует базовые методы исключений проекта
+ *
+ * @package XEAF\API\Core
+ */
+abstract class Exception extends \Exception {
+
+}
---
+++ tests/unit/api/core/ExceptionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * ExceptionTest.php
+ *
+ * Файл является неотъемлемой частью проекта XEAF-PHP
+ *
+ * @author    Николай В. Анохин <n.anokhin@xeaf.net>
+ * @copyright 2019 XEAF.NET Group
+ *
+ * @license   Apache 2.0
+ */
+namespace Tests\XEAF\API\Core;
+
+use Codeception\Test\Unit;
+
+/**
+ * @covers  \XEAF\API\Core\Exception
+ *
+ * @package Tests\XEAF\API\Core
+ */
+class ExceptionTest extends Unit {
+
+    /**
+     *
+     *
+     * @return void
+     */
+    public function testDemo(): void {
+        $this->assertSame(true, true);
+    }
+}
