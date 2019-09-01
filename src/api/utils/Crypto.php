cdbb06151fe9416d10455105d6cd5d2c3c14218c
n.anokhin <n.anokhin@xeaf.net>
2019-09-01 23:38:26 +0300

    Добавлен класс XEAF\API\Utils\Crypto

---
+++ src/api/utils/Crypto.php
@@ -0,0 +1,143 @@
+<?php
+
+/**
+ * Crypto.php
+ *
+ * Файл является неотъемлемой частью проекта XEAF-PHP
+ *
+ * @author    Николай В. Анохин <n.anokhin@xeaf.net>
+ * @copyright 2019 XEAF.NET Group
+ *
+ * @license   Apache 2.0
+ */
+namespace XEAF\API\Utils;
+
+use XEAF\API\App\Factory;
+use XEAF\API\Core\FactoryObject;
+use XEAF\API\Utils\Interfaces\ICrypto;
+
+class Crypto extends FactoryObject implements ICrypto {
+
+    /**
+     * Идентификатор алгоритма построения хеша данных
+     */
+    private const HASH_ALGO = 'sha256';
+
+    /**
+     * Идентификатор алгоритма построения хеша паролей
+     */
+    private const PASSWORD_ALGO = PASSWORD_DEFAULT;
+
+    /**
+     * Возвращает идентификатор алгоритма построения хеша данных
+     *
+     * @return string
+     */
+    public function getHashAlgo(): string {
+        return self::HASH_ALGO;
+    }
+
+    /**
+     * Возвращает идентификатор алгоритма построения хеша паролей
+     *
+     * @return string
+     */
+    public function getPasswordAlgo(): string {
+        return self::PASSWORD_ALGO;
+    }
+
+    /**
+     * Генерирует хеш на основе пароля и строковых данных
+     *
+     * @param string $data     Данные для хеша
+     * @param string $password Пароль
+     *
+     * @return string
+     */
+    public function hash(string $data, $password = '') {
+        return hash_hmac($this->getHashAlgo(), $data, $password);
+    }
+
+    /**
+     * Безопасный метод сравнение хешей
+     *
+     * @param string $hash1 Известный хеш
+     * @param string $hash2 Сравниваемый хеш
+     *
+     * @return bool
+     */
+    public function hashEquals(string $hash1, string $hash2): bool {
+        return hash_equals($hash1, $hash2);
+    }
+
+    /**
+     * Возвращает случайно сгенерированную последовательность байтов
+     *
+     * @param int $length Длина строки
+     *
+     * @return string
+     */
+    public function randomBytes(int $length): string {
+        $result = '';
+        for ($i = 0; $i < $length; $i++) {
+            $result .= chr(mt_rand(0, 255));
+        }
+        return $result;
+    }
+
+    /**
+     * Генерирует UUID версии 4
+     *
+     * @return string
+     */
+    public function generateUUIDv4(): string {
+        $data    = self::randomBytes(16);
+        $data[6] = chr(ord($data[6]) & 0x0f | 0x40); // Установка версии в 0100
+        $data[8] = chr(ord($data[8]) & 0x3f | 0x80); // Установка 6-7 битов в to 10
+        return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
+    }
+
+    /**
+     * Возвращает хеш пароля
+     *
+     * @param string $password Пароль
+     *
+     * @return string
+     */
+    public function passwordHash(string $password): string {
+        return password_hash($password, $this->getPasswordAlgo());
+    }
+
+    /**
+     * Проверяет соответствие пароля хешу
+     *
+     * @param string $password Пароль
+     * @param string $hash     Хеш пароля
+     *
+     * @return bool
+     */
+    public function verifyPassword(string $password, string $hash): bool {
+        return password_verify($password, $hash);
+    }
+
+    /**
+     * Генерирует значение токена безопасности
+     *
+     * @return string
+     */
+    public function securityToken(): string {
+        $uuid = Crypto::generateUUIDv4();
+        return base64_encode($uuid);
+    }
+
+    /**
+     * Возвращает единичный экемпляр объекта
+     *
+     * @return \XEAF\API\Utils\Interfaces\ICrypto
+     */
+    public static function getInstance(): ICrypto {
+        $result = Factory::getFactoryObject(Crypto::class, self::DEFAULT_NAME);
+        assert($result instanceof ICrypto);
+        return $result;
+    }
+}
